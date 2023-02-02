df5794d836ed788e5238180627682d6616b759a5
ehdgud1919 <rlaehdgud55@naver.com>
2023-02-02 16:48:30 +0900

    2023-02-02

--- Day01_03_테이블조회하기.sql
+++ Day01_03_테이블조회하기.sql
@@ -65,9 +65,55 @@ SELECT *
 SELECT EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO
   FROM EMP;
   
--- 3) 동일한 데이터는 한 번만 조회하기
+-- 3. 동일한 데이터는 한 번만 조회하기
 SELECT DISTINCT JOB
   FROM EMP;
+
+
+
+-- 4. JOB이 MANAGER인 사원 목록 조회하기
+SELECT EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO
+  FROM EMP
+ WHERE JOB = 'MANAGER';         -- DB에선 "=" ← 같다라는 뜻 
+ 
+SELECT EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO
+  FROM EMP
+ WHERE JOB IN('MANAGER');       
+
+-- 5.SAL이 1500 초과인 사원 목록 조회하기
+SELECT EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO
+  FROM EMP
+ WHERE SAL > 1500;  
+
+-- 6. SAL이 2000 ~ 2999 사이인 사원 목록 조회하기
+SELECT EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO
+  FROM EMP
+ WHERE SAL BETWEEN 2000 AND 2999;  
+
+-- 7. COMM을 받는 사원 목록 조회하기
+-- NULL 체크하는 코드
+--  1) NULL 이다      : IS NULL
+--  2) NULL 아니다    : IS NOT NULL
+SELECT EMPNO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DEPTNO
+  FROM EMP
+ WHERE COMM IS NOT NULL 
+   AND COMM != 0;
+
+
+-- 8. ENAME이 A로 시작하는 사원 목록 조회하기  
+--    1) WILD CARD
+--      (1) %   : 글자 수 제한 없는 모든 문자  주로 사용
+--      (2) _   : 1글자로 제한 된 모든 문자
+--    2) 연산자
+--      (1) LIKE        : WILD CARD를 포함한다.
+--      (2) NOT LIKE    : WILD CARD를 포함하지 않는다.
+--          A%      : A로 시작하는 모든 문자
+--          %A      : A로 끝나는 모든 문자
+--          %A%     :  A가 어디에든 들어가는 모든 문자
+SELECT EMPNO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DEPTNO
+  FROM EMP
+ WHERE ENAME LIKE 'A%';  
+
 
 
 
