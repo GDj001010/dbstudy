f2e5363f04bcc2ad9ac4d5be967e195da5942729
ehdgud1919 <rlaehdgud55@naver.com>
2023-02-06 12:44:31 +0900

    2023-02-06

--- Day02_01_KEY(제품,주문).sql
+++ Day02_01_KEY(제품,주문).sql
@@ -16,7 +16,28 @@
         1) 생성 규칙 : "반드시" 부모 테이블을 먼저 생성한다.
         2) 삭제 규칙 : "반드시" 자식 테이블을 먼저 삭제한다.
 */
+/*
+    외래키 제약조건 옵션
+    부모가 지워졌을 때 "참조무결성" 위배가 나옴 - FK
+    1. 부모가 지워졌을 때를 대비한 옵션 ONDELETE
+        1) 부모가 죽으면 나도 죽는다 ( 같이 지워버림 ) 
+            CASCADE
+            ON DELETE CASCADE
+        2) 부모가 죽으면 NULL 처리 한다 (주문내역은 살리면서 FK 칸만 NULL 처리 )
+            SET NULL
+            ON DELETE SET NULL
+*/
+/*
+    외래키 제약 조건의 옵션
+    1. ON DELETE CASCADE  실선
+        1) 참조 중인 PARENT KEY가 삭제되면 해당 PARENT KEY를 가진 행 전체를 함께 삭제한다.
+        2) 예시) 회원 탈퇴 시 작성한 모든 게시글이 함께 삭제됩니다.
+                 게시글 삭제 시 해당 게시글에 달린 모든 댓글이 함께 삭제됩니다.
+    2. ON DELETE SET NULL  점선
+        1) 참조 중인 PARENT KEY가 삭제되면 해당 PARENT KEY를 가진 칼럼 값만 NULL로 처리한다.
+        2) 예시) 어떤 상품을 제거하였으나 해당 상품의 주문 내역은 남아 있는 경우
 
+*/
 -- 테이블 삭제               삭제를 몰아서 위쪽에 배치, 생성은 아래쪽 배치
 DROP TABLE ORDER_TBL;           -- 자식 테이블 먼저 삭제 후 부모 테이블 삭제
 DROP TABLE PRODUCT_TBL;
@@ -37,10 +58,8 @@ CREATE TABLE ORDER_TBL (
     PROD_NO NUMBER , 
     ORDER_DATE DATE,
     CONSTRAINT PK_ORDER PRIMARY KEY(ORDER_NO),    -- PK 기본키
-    CONSTRAINT FK_ORDER_PROD FOREIGN KEY(PROD_NO) REFERENCES PRODUCT_TBL(PROD_NO)  -- FK 외래키
+    CONSTRAINT FK_ORDER_PROD FOREIGN KEY(PROD_NO) REFERENCES PRODUCT_TBL(PROD_NO) ON DELETE CASCADE  -- FK 외래키
 );
-
-
 
 -- CONSTRAINT : 제약조건
 -- REFERENCES : 참조
